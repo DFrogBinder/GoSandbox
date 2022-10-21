cbe101cdbbc85d157d87aa5754a77730b518e218
Boyan.Ivanov <boyan.ivanov@perspectum.com>
2022-10-21 13:20:31 +0100

    Have results.....might be a bit wrong

--- Parse.go
+++ Parse.go
@@ -26,14 +26,12 @@ type Card struct {
 	SequenceVal   []string `xml:"ProtParameter>ValueAndUnit"`
 }
 
-func Compare_Inner_Structure(FR []string, SR []string) ([]int, []string) {
-	var SimilarityList []int
-	var WrongParameters []string
+func Compare_Inner_Structure(FR []string, SR []string, SimilarityList []int, WrongParameters []string) ([]int, []string) {
 	if FR[1] == SR[1] && FR[2] == SR[2] {
 		if FR[3] == SR[3] {
 			SimilarityList = append(SimilarityList, 1)
 		} else {
-			msg := fmt.Sprintln("In: ", SR[0], SR[1], "Found: ", SR[2], SR[3], " => Should be:", FR[3])
+			msg := fmt.Sprintln("In: ", SR[0], SR[1], "=> Found: ", SR[2], SR[3], " => Should be:", FR[3])
 			WrongParameters = append(WrongParameters, msg)
 		}
 	}
@@ -71,11 +69,11 @@ func CompareReports(FR, SR []string) ([]int, []string) {
 	var SimilarityList []int
 	var WrongParameters []string
 
-	var T2StarAlias = []string{"LMS_T2Star", "LMS T2STAR DIXON", "LMS_T2STAR", "LMS T2STAR"}
-	var ExcludeAlias = []string{"Pancreas", "pancreas", "kidney", "Kidney", "localizer_haste_bh"}
-	var IdealALias = []string{"LMS_IDEAL", "LMS IDEAL", "LMS Ideal", "LMS IDEAL"}
-	var MolliAlias = []string{"LMS_MOLLI", "LMS MOLLI", "LMS Molli", "LMS_Molli"}
-	var MostAlias = []string{"LMS MOST", "LMS Most", "LMS_MOST", "LMS_Most"}
+	var T2StarAlias = []string{"T2STAR DIXON", "LMS_T2Star", "LMS T2STAR DIXON", "LMS_T2STAR", "LMS T2STAR"}
+	var ExcludeAlias = []string{"Pancreas", "pancreas", "kidney", "Kidney", "localizer", "VOLUME"}
+	var IdealALias = []string{"IDEAL", "LMS_IDEAL", "LMS IDEAL", "LMS Ideal", "LMS IDEAL"}
+	var MolliAlias = []string{"MOLLI", "LMS_MOLLI", "LMS MOLLI", "LMS Molli", "LMS_Molli"}
+	var MostAlias = []string{"MOST", "LMS MOST", "LMS Most", "LMS_MOST", "LMS_Most"}
 
 	for i := range FR {
 		for j := range SR {
@@ -87,24 +85,24 @@ func CompareReports(FR, SR []string) ([]int, []string) {
 				contains(T2StarAlias, tSR[0]) &&
 				!contains(ExcludeAlias, tFR[0]) &&
 				!contains(ExcludeAlias, tSR[0]) {
-				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR)
+				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR, SimilarityList, WrongParameters)
 				// Ideal Area
 			} else if contains(IdealALias, tFR[0]) &&
 				contains(IdealALias, tSR[0]) &&
 				!contains(ExcludeAlias, tFR[0]) &&
 				!contains(ExcludeAlias, tSR[0]) {
-				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR)
+				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR, SimilarityList, WrongParameters)
 				// Molli Area
 			} else if contains(MolliAlias, tFR[0]) &&
 				contains(MolliAlias, tSR[0]) &&
 				!contains(ExcludeAlias, tFR[0]) &&
 				!contains(ExcludeAlias, tSR[0]) {
-				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR)
+				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR, SimilarityList, WrongParameters)
 			} else if contains(MostAlias, tFR[0]) &&
 				contains(MostAlias, tSR[0]) &&
 				!contains(ExcludeAlias, tFR[0]) &&
 				!contains(ExcludeAlias, tSR[0]) {
-				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR)
+				SimilarityList, WrongParameters = Compare_Inner_Structure(tFR, tSR, SimilarityList, WrongParameters)
 			}
 		}
 	}
@@ -158,6 +156,6 @@ func main() {
 	fmt.Println("Number of parameters in the first report: " + fmt.Sprint(len(SecondReportParamsList)))
 
 	Similarity, Message := CompareReports(FirstReportParamsList, SecondReportParamsList)
-	fmt.Println(len(Similarity))
+	fmt.Println("Correct Paramters: ", len(Similarity))
 	fmt.Println(Message)
 }
